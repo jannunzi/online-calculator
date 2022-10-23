afcdb7a8f7a28f33bffcb5460ed5ebc1959fcad1
JoseAnnunziatoSonos <Jose.Annunziato@sonos.com>
2022-10-23 17:15:39 -0400

    implemented add HTTP endpoint

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/hello', (req, res) => res.send('Hello World'))
+app.get('/add/:a/:b', (req, res) =>
+  res.send(parseInt(req.params.a) +
+           parseInt(req.params.b)))
 
 app.listen(4000)
