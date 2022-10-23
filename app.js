3236fbd04d1c09ac958e8e6b019e932cb8d2abd0
JoseAnnunziatoSonos <Jose.Annunziato@sonos.com>
2022-10-23 17:29:54 -0400

    implemented subtract feature

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express()
 app.get('/hello', (req, res) => res.send('Hello World'))
 app.get('/add/:a/:b', (req, res) =>
   res.send(parseInt(req.params.a) +
-           parseInt(req.params.b)))
+    parseInt(req.params.b)))
+app.get('/subtract/:a/:b', (req, res) =>
+  res.send(parseInt(req.params.a) -
+    parseInt(req.params.b)))
 
 app.listen(4000)
