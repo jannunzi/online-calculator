e45c862e4e073286c98afd3797531594ae81995c
jannunzi <jannunzi@gmail.com>
2022-10-24 22:09:29 -0400

    fixed text response

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express()
 
 app.get('/hello', (req, res) => res.send('Hello World'))
 app.get('/add/:a/:b', (req, res) =>
-  res.send(parseInt(req.params.a) +
+  res.text(parseInt(req.params.a) +
     parseInt(req.params.b)))
 app.get('/subtract/:a/:b', (req, res) =>
-  res.send(parseInt(req.params.a) -
+  res.text(parseInt(req.params.a) -
     parseInt(req.params.b)))
 
 app.listen(4000)
