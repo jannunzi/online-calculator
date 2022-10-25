7322043233bd15100d940d901e64a8083daa3160
JgaAws <jgaaws@gmail.com>
2022-10-24 22:15:04 -0400

    added brackets

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello World'))
-app.get('/add/:a/:b', (req, res) =>
-  res.send(parseInt(req.params.a) +
-    parseInt(req.params.b)))
-app.get('/subtract/:a/:b', (req, res) =>
-  res.send(parseInt(req.params.a) -
-    parseInt(req.params.b)))
+app.get('/hello', (req, res) => {
+  res.send('Hello World')
+})
+app.get('/add/:a/:b', (req, res) => {
+  const c = parseInt(req.params.a) + parseInt(req.params.b)
+  res.send(c)
+})
+app.get('/subtract/:a/:b', (req, res) => {
+  const c = parseInt(req.params.a) - parseInt(req.params.b)
+  res.send(c)
+})
 
 app.listen(4000)
